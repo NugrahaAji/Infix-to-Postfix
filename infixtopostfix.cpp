9d92aecfa41df75f18310074c3edd9a9f4da9a69
nugrahaahi <ngrhaji31@gmail.com>
2024-05-29 18:46:19 +0700

    menambahakan fungsi infixToPostfix dan memperbaiki sedikit bug pada function strToInfix

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -26,7 +26,7 @@ vector<string> strToInfix(string input){
 				infix.push_back(tmp);
 				tmp ="";
 			}
-			if(input[i]=='_'&&(i==0 || !isdigit(input[i-1]))){
+			if(input[i]=='-'&&(i==0 || !isdigit(input[i-1]))){
 				if(input[i-1]!=')'){
 					if(i+1<len && (input[i+1]=='(' || input[i-1]== '(')){
 						infix.push_back("-1");
@@ -40,27 +40,64 @@ vector<string> strToInfix(string input){
 			    	infix.pop_back();
 			    	infix.push_back("+");
 			    }
-		     	else if(i==0)tmp += input[i];
+		     	else if(i==0) tmp += input[i];
+				
 		     	else infix.push_back(string(1,input[i]));
+	    		}
+		    	else if(input[i-1]==')'){
+		    		infix.push_back(string(1,input[i]));
+		    	}
 	    	}
-		    else if(input[i-1]==')'){
-		    	infix.push_back(string(1,input[i]));
-		    }
-	    }
-	    else{
-	    	infix.push_back(string(1,input[i]));
-	    }
-    }
+	    	else{
+	    		infix.push_back(string(1,input[i]));
+	    	}
+    	}
+	}
+
+	if 	(!tmp.empty()){
+		infix.push_back(tmp);
+	}
+	return infix;
 }
-if (!tmp.empty()){
-	infix.push_back(tmp);
-}
-return infix;
+
+vector<string> infixToPostfix(vector<string> infix){
+	vector<string> postfix;
+	vector<string> oprnd;
+	string tmp;
+
+	for(auto itr = infix.begin(); itr != infix.end(); itr++){
+		tmp = *itr;
+		if(isdigit(tmp[0]) || (tmp[0] == '-' && isdigit(tmp[1]) && tmp.size() > 1)){
+			postfix.push_back(tmp);
+		}
+		else if(tmp == "("){
+			oprnd.push_back(tmp);
+		}
+		else if(tmp == ")"){
+			while(!oprnd.empty() && oprnd.back() != "("){
+				postfix.push_back(oprnd.back());
+				oprnd.pop_back();
+			}
+			oprnd.pop_back();
+		}
+		else{
+			while(!oprnd.empty() && operand(oprnd.back()) >= operand(tmp)){
+				postfix.push_back(oprnd.back());
+				oprnd.pop_back();
+			}
+			oprnd.push_back(tmp);
+		}
+	}
+	while(!oprnd.empty()){
+		postfix.push_back(oprnd.back());
+		oprnd.pop_back();
+	}
+	return postfix;
 }
 
 void print(vector<string>&string){
 	for(auto itr = string.begin(); itr!= string.end(); itr++){
-		cout << *itr << "";
+		cout << *itr << " ";
 	}
 	cout << endl;
 }
@@ -68,8 +105,9 @@ void print(vector<string>&string){
 int main(){
 	string input;
 	getline(cin,input);
-	vector<string>infix = strToInfix(input);
-	print(infix);
+	vector<string> infix = strToInfix(input);
+	vector<string> postfix = infixToPostfix(infix);
+	print(postfix);
 	
 	return 0;	
 }
